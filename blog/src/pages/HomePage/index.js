900de44adcb54761f92aab73f7b457841cc171cc
tourcoder <code@tourcoder.com>
2023-06-27 16:08:12 +0800

    更新内容

--- blog/src/pages/HomePage/index.js
+++ blog/src/pages/HomePage/index.js
@@ -1,20 +1,21 @@
 import React from "react";
 import { Fragment } from "react";
+import styles from './homepage.module.css';
 
 function HomePage() {
     return (
         <Fragment>
-            <div className="topbar">
+            <div className={styles.topbar}>
                 <h1>Blog</h1>
                 <a href="/login">Login</a>
             </div>
-            <ul className="post-list">
+            <ul className={styles.postlist}>
                 <li><a href="/detail">Title</a></li>
                 <li><a href="/detail">Title</a></li>
                 <li><a href="/detail">Title</a></li>
             </ul>
         </Fragment>
     );
-  }
+}
   
-  export default HomePage;
+export default HomePage;
